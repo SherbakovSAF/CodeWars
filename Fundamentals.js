fc05d36a12ac8a6fe917e0860be990ee2e039589
SherbakovSAF <sherbakovsaf@gmail.com>
2022-09-09 21:40:05 +0300

    +1 task

--- Fundamentals.js
+++ Fundamentals.js
@@ -1,3 +1,10 @@
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+function greet(name){
+     return `Hello, ${name} how are you doing today?`
+}
+console.log(greet(ВАся))
+
 // Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 function digitize(n) {
      return [...n.toString()].reverse().map(Number);
