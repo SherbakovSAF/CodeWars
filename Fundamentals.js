d71c4db1caade3a8225338a8140981b8435722d0
SherbakovSAF <sherbakovsaf@gmail.com>
2022-09-10 21:06:21 +0300

    +1 task (Total: 4)

--- Fundamentals.js
+++ Fundamentals.js
@@ -1,9 +1,16 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+// Examples:
+function opposite(number) {
+     return number * -1
+   }
+
+console.log(opposite(32))
+createPhoneNumber([1,2,4,5,6,6,7,7,6,1])
 // Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
 // [Make sure you type the exact thing I wrote or the program may not execute properly]
 function greet(name){
      return `Hello, ${name} how are you doing today?`
 }
-console.log(greet(ВАся))
 
 // Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 function digitize(n) {
