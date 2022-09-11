0ee62c61101045bba04d39142a152b59cb83a19a
SherbakovSAF <sherbakovsaf@gmail.com>
2022-09-11 20:52:17 +0300

    +1 task (Total: 7)

--- Fundamentals.js
+++ Fundamentals.js
@@ -1,3 +1,18 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+function reverseWords(str) {
+     let arr = str.split(' ').reverse()
+     let arrItog = [];
+     for (i=0; i<arr.length; i++) {
+          arrItog.push(arr[i].split('').reverse().join(''))
+     }
+     return arrItog.reverse()
+   }
+
+   console.log(reverseWords('Привет как дела?'))
+
 // Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
 // Example
 // createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
@@ -16,7 +31,6 @@ function createPhoneNumber(numbers){
      }
 }
 
-console.log(createPhoneNumber([1,2,4,6,3,4,3,3,1,4]))
 
 // Write function bmi that calculates body mass index (bmi = weight / height2).
 // if bmi <= 18.5 return "Underweight"
