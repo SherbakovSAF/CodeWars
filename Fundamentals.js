a267db65d24f10e6628533700755b2c736b6fe89
SherbakovSAF <sherbakovsaf@gmail.com>
2022-09-15 14:16:02 +0300

    +1 task (Total: 15)
    Решение найдено в интернете

--- Fundamentals.js
+++ Fundamentals.js
@@ -1,10 +1,27 @@
+// A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+function isPangram(string) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const regex = /\s/g;
+    const lowercase = string.toLowerCase().replace(regex, "");
+
+    for (let i = 0; i < alphabet.length; i++) {
+        if (lowercase.indexOf(alphabet[i]) === -1) {
+            return "not pangram";
+        }
+    }
+
+    return "pangram";
+}
+
+ console.log(isPangram('The quick brown fox jumps over the lazy dog'))  
 // Implement a function which convert the given boolean value into its string representation.
 // Note: Only valid inputs will be given.
 function booleanToString(b){
      return b.toString()
    }
 
-console.log(booleanToString(true))
 // Given a list of integers, determine whether the sum of its elements is odd or even.
 // Give your answer as a string matching "odd" or "even".
 // If the input array is empty consider it as: [0] (array with a zero).
