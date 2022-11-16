1043f576811a2ecf3110b15fd255433cbafe86a9
SherbakovSAF <sherbakovsaf@gmail.com>
2022-11-16 23:55:41 +0300

    +1 task (Total: 21)

--- Fundamentals.js
+++ Fundamentals.js
@@ -1,3 +1,19 @@
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+function century(year) {
+    return Math.ceil(year / 100);
+}
+
 // Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
 
 // invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
