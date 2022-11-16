a961f8efb9d9f46d74d638c0839eeb0d69318ba0
SherbakovSAF <sherbakovsaf@gmail.com>
2022-11-16 23:30:37 +0300

    +1 task (Total: 18)

--- Fundamentals.js
+++ Fundamentals.js
@@ -1,3 +1,18 @@
+ // In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+ // Examples
+ // makeNegative(1);    // return -1
+ // makeNegative(-5);   // return -5
+ // makeNegative(0);    // return 0
+ // makeNegative(0.12); // return -0.12
+ // Notes
+ // The number can be negative already, in which case no change is required.
+ // Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+ 
+function makeNegative(num) {
+     return num >=0 ? num * -1 : num
+}
+
+
 // The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
 
 // To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
