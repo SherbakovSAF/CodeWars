9d74557fab8e45db0156885af8c812195dccee73
SherbakovSAF <sherbakovsaf@gmail.com>
2022-11-16 23:31:00 +0300

    +1 task (Total: 19)

--- Fundamentals.js
+++ Fundamentals.js
@@ -1,4 +1,19 @@
- // In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+
+// Can you find the needle in the haystack?
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+// After your function finds the needle it should return a message (as a string) that says:
+// "found the needle at position " plus the index it found the needle, so:
+// Example(Input --> Output)
+// ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+// Note: In COBOL, it should return "found the needle at position 6"
+
+function findNeedle(haystack) {
+     return "found the needle at position " + haystack.indexOf("needle");
+ }
+ 
+ 
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
  // Examples
  // makeNegative(1);    // return -1
  // makeNegative(-5);   // return -5
