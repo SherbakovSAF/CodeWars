11678c7c28faa29ec7b29afb22133d2b8d53de22
SherbakovSAF <sherbakovsaf@gmail.com>
2022-12-25 23:53:23 +0300

    +1 task (Total: 33)

--- Fundamentals.js
+++ Fundamentals.js
@@ -1,3 +1,16 @@
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+// Don't change the order of the elements that are left.
+// Examples
+// * Input: [1,2,3,4,5], output = [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+function removeSmallest(numbers) {
+     let localArray = [...numbers]
+     localArray.splice(localArray.indexOf(Math.min(...localArray)), 1)
+     return localArray
+   }
+
 // Complete the square sum function so that it squares each number passed into it and then sums the results together.
 // For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
